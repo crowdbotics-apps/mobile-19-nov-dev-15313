feb88ae41d3fb366e3e7bbdb43758643edbcdc26
Crowdbotics <team@crowdbotics.com>
2020-11-19 06:56:52 +0000

    Update components

--- src/features/BlankScreen014686/screens/blank.js
+++ src/features/BlankScreen014686/screens/blank.js
@@ -23,56 +23,15 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {
-    TextInput_4: "",
-    TextInput_5: "",
-    TextInput_6: "",
-    Switch_7: true,
-    CheckBox_8: true,
-    CheckBox_9: true
-  }
+  state = {}
 
   render = () => (
     <View>
+      <Text style={styles.Text_3}>Sample text content</Text>
       <Button
         title="Press me!"
         style={styles.Button_2}
         onPress={() => alert("Pressed!")}
-      />
-      <Text style={styles.Text_3}>Sample text content</Text>
-      <TextInput
-        placeholder="Sample text input placeholder"
-        style={styles.TextInput_4}
-        value={this.state.TextInput_4}
-        onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
-      />
-      <TextInput
-        placeholder="Number Input Placeholder"
-        style={styles.TextInput_5}
-        value={this.state.TextInput_5}
-        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
-      />
-      <TextInput
-        placeholder="Sample text input placeholder"
-        style={styles.TextInput_6}
-        value={this.state.TextInput_6}
-        onChangeText={nextValue => this.setState({ TextInput_6: nextValue })}
-      />
-      <Switch
-        style={styles.Switch_7}
-        value={this.state.Switch_7}
-        onValueChange={nextChecked => this.setState({ Switch_7: nextChecked })}
-      />
-      <CheckBox
-        title="Radio button"
-        containerStyle={styles.CheckBox_8}
-        checked={this.state.CheckBox_8}
-        onPress={nextChecked => this.setState({ CheckBox_8: nextChecked })}
-      />
-      <CheckBox
-        title="Checkbox"
-        checked={this.state.CheckBox_9}
-        onPress={nextChecked => this.setState({ CheckBox_9: nextChecked })}
       />
     </View>
   )
@@ -138,5 +97,9 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     letterSpacing: 6
   },
-  CheckBox_9: {}
+
+  CheckBox_9: {},
+  View_1: {},
+  Text_3: { width: "100%" },
+  Button_2: { textAlign: "right", textAlignVertical: "top" }
 })
